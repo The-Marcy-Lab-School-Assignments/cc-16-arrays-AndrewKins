e76793e045a5280262d55c2a6a72004c983ef811
Andrew Enoe <andrewm.enoe@gmail.com>
2021-11-16 16:34:36 +0000

    finished

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-//write your code here
+function replaceWithYerr(array, startIndex, deleteCount){
+    let newArray = [...array] //make a copy of the OG array
+    if(startIndex === undefined && deleteCount === undefined){
+        //replace all elements in newArray with yerr
+        for(let i = 0; i < newArray.length; i++){
+            newArray[i] = 'yerr'
+        }
+    } else if(deleteCount === undefined){
+        //replace all elements in newArray starting at startIndex
+        for(let i = startIndex; i < newArray.length; i++){
+            newArray[i] = 'yerr'
+        }
+    } else {
+        //then I have 3 arguments
+        for(let i = startIndex; i < deleteCount + startIndex; i++){
+            newArray[i] = 'yerr'
+        }
+    }
+
+    return newArray
+}
